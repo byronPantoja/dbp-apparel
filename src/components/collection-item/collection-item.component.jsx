1a3def9258d5cd3505834de2b77197096e21d73d
dev833 <byronpantoja@gmail.com>
2020-04-24 13:57:10 +0800

    Adding logic and styles into collection-item files

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
-import './collection-item.style.scss';
+import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, price, imageUrl }) => (
+const CollectionItem = ({ id, name, price, imageUrl }) => (
 	<div className='collection-item'>
 		<div
 			className='image'
 			style={{
 				backgroundImage: `url(${imageUrl})`,
 			}}
-		>
-			<div className='collection-footer'>
-				<span className='name'>{name}</span>
-				<span className='price'>{price}</span>
-			</div>
+		/>
+		<div className='collection-footer'>
+			<span className='name'>{name}</span>
+			<span className='price'>{price}</span>
 		</div>
 	</div>
 );
