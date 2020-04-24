ee449967f443c0a2af81eba95b56879c8a72fdeb
dev833 <byronpantoja@gmail.com>
2020-04-24 16:21:30 +0800

    Updates

---
+++ precache-manifest.6b8951ae710618b37e84d3eb36dca234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eeaeb58d7699f0fb691d58571cb70ba",
+    "url": "/dbp-apparel/index.html"
+  },
+  {
+    "revision": "70a0a3d0970cb057db59",
+    "url": "/dbp-apparel/static/css/main.5b1eb01c.chunk.css"
+  },
+  {
+    "revision": "cc39043fe6a5c9686abb",
+    "url": "/dbp-apparel/static/js/2.042cb196.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dbp-apparel/static/js/2.042cb196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a0a3d0970cb057db59",
+    "url": "/dbp-apparel/static/js/main.47c51bf0.chunk.js"
+  },
+  {
+    "revision": "12d2e5b179c511c8dfd1",
+    "url": "/dbp-apparel/static/js/runtime-main.271af214.js"
+  }
+]);
